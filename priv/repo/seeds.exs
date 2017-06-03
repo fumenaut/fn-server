e5b005b033bfd8dd0c9244b6ec31b79b4e62b42f
Allen Gooch <allen.gooch@gmail.com>
2017-06-03 01:04:06 -0500

    Add seed data for cigar countries and manufacturers.

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,6 +10,8 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 alias Fumenaut.Account.User
+alias Fumenaut.Cigar.Country
+alias Fumenaut.Cigar.Manufacturer
 alias Fumenaut.Journal.Smoke
 alias Fumenaut.Repo
 
@@ -34,3 +36,24 @@ for _ <- 1..7 do
       user_id: bob.id
     })
 end
+
+Repo.insert!(%Country{name: "Dominican Republic"})
+Repo.insert!(%Country{name: "Nicaragua"})
+Repo.insert!(%Country{name: "Costa Rica"})
+Repo.insert!(%Country{name: "Honduras"})
+Repo.insert!(%Country{name: "Brazil"})
+Repo.insert!(%Country{name: "Mexico"})
+Repo.insert!(%Country{name: "United States"})
+
+Repo.insert!(%Manufacturer{name: "Arturo Fuentes"})
+Repo.insert!(%Manufacturer{name: "Drew Estates"})
+Repo.insert!(%Manufacturer{name: "Padron"})
+Repo.insert!(%Manufacturer{name: "Illusione"})
+Repo.insert!(%Manufacturer{name: "My Father"})
+Repo.insert!(%Manufacturer{name: "Tatuaje"})
+Repo.insert!(%Manufacturer{name: "Davidoff"})
+Repo.insert!(%Manufacturer{name: "Perdomo"})
+Repo.insert!(%Manufacturer{name: "Joya de Nicaragua"})
+Repo.insert!(%Manufacturer{name: "Ashton"})
+Repo.insert!(%Manufacturer{name: "San Cristobal"})
+Repo.insert!(%Manufacturer{name: "Caldwell"})
